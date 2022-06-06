c3a3f9dd9e3fc4e10a5919baeb668c4f4bfebe4f
James Bradbury <github@jamesbradbury.net>
2022-06-06 17:49:08 +0100

    beef up parser to handle folders

--- parser.js
+++ parser.js
@@ -5,8 +5,51 @@ const path = require('path');
 const fg = require('fast-glob');
 const Mustache = require('mustache');
 
-const data = fs.readFileSync('./fluid.list2buf.toml');
-const parsed = toml.parse(data);
+const dir = './output';
+
+const sanitise = (data) => {
+    // Messages
+    let copy = data;
+    let messages = []
+    if (data.messages) {
+        for (const [k, v] of Object.entries(data.messages)) {
+            let message = v;
+            message.name = k;
+            messages.push(message)
+        }
+    }
+    // Attributes
+    let attributes = []
+    if (data.attributes) {
+        for (const [k, v] of Object.entries(data.attributes)) {
+            let attr = v;
+            attr.name = k;
+            attributes.push(attr)
+        }
+    }
+    // Inlets
+    // Outlets
+    copy.messages = messages
+    copy.attributes = attributes
+    return copy
+}
+
+const configs = fg.sync(['docs/*.toml']);
+const template = fs.readFileSync('./template.mustache', 'utf8')
+Mustache.escape = (text) => text; // escape maxref tags
+
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+for (const config of configs) {
+    const data = fs.readFileSync(config);
+    const parsed = sanitise(toml.parse(data));
+    const output = path.join(dir, path.parse(config).name + '.maxref.xml');
+    fs.writeFileSync(output, Mustache.render(template, parsed))
+    console.log('Wrote ' + output);
+}
 
 
-// replace `` with <m> </m>
+
+
