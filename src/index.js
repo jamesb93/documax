e40734b51d2aa7e2405f7eef26c73ad1ee2648fa
James Bradbury <github@jamesbradbury.net>
2022-09-03 02:08:43 +0100

    simplify default handling

--- src/index.js
+++ src/index.js
@@ -39,54 +39,26 @@ const dir = options.output;
 const cce = options.environment;
 const templatePath = options.template;
 const extensions = new Map([['max', '.maxref.xml']]);
+
+const insertDefault = (source, destination, key) => {
+    if (!source) return
+    Object.entries(source).forEach(([k, v]) => {
+        let temp = v;
+        temp[key] = k;
+        destination.push(temp)
+    })
+}
 const sanitise = (data) => {
-    // Messages
     let copy = data;
     let messages = []
-    if (data.messages) {
-        for (const [k, v] of Object.entries(data.messages)) {
-            let message = v;
-            message.name = k;
-            messages.push(message)
-        }
-    }
-    // Attributes
     let attributes = []
-    if (data.attributes) {
-        for (const [k, v] of Object.entries(data.attributes)) {
-            let attr = v;
-            attr.name = k;
-            attributes.push(attr)
-        }
-    }
-
     let arguments = []
-    if (data.arguments) {
-        for (const [k, v] of Object.entries(data.arguments)) {
-            let arg = v;
-            arg.name = k;
-            arguments.push(arg)
-        }
-    }
-
-    // Inlets
     let inlets = []
-    if (data.inlets) {
-        for (const [k, v] of Object.entries(data.inlets)) {
-            let inlet = v;
-            inlet.id = k;
-            inlets.push(inlet)
-        }
-    }
-    // Outlets
     let outlets = []
-    if (data.outlets) {
-        for (const [k, v] of Object.entries(data.outlets)) {
-            let outlet = v;
-            outlet.id = k;
-            outlets.push(outlet)
-        }
-    }
+    insertDefault(data.messages, messages, 'name');
+    insertDefault(data.arguments, arguments, 'name');
+    insertDefault(data.inlets, inlets, 'id');
+    insertDefault(data.outlets, outlets, 'id');
     copy.messages = messages
     copy.attributes = attributes
     copy.arguments = arguments
